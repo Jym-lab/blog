bf8b419ace5464f463cc98acf25cc200daa8ea20
Jym-lab <jym9809@gmail.com>
2024-08-14 10:45:21 +0900

    feat: post count by categories

--- lib/contentsManager.ts
+++ lib/contentsManager.ts
@@ -4,6 +4,20 @@ import { POSTS_DIR } from '@/utils/paths';
 import fs from 'fs'
 import path from 'path'
 
+export const countPostsByCategory = (posts: string[]) => {
+    const countMap: Record<string, number> = {};
+
+    posts.forEach(post => {
+        let category = path.dirname(path.relative(POSTS_DIR, post));
+        while (category !== '.') {
+            countMap[category] = (countMap[category] || 0) + 1;
+            category = path.dirname(category);
+        }
+    });
+
+    return countMap;
+};
+
 const fetchAllFiles = (dir: string, depth = 0, maxDepth = 2, posts: string[] = [], categories: string[] = []) => {
     if (depth > maxDepth) return {categories, posts};
     fs.readdirSync(dir).forEach((file) => {
